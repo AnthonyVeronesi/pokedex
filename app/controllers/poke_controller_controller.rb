83063d25919c407823320d38460b765a3d825111
Anthony Veronesi <tuffstuffmagee@gmail.com>
2019-06-18 02:33:58 -0400

    initial commit

---
+++ app/controllers/poke_controller_controller.rb
@@ -0,0 +1,2 @@
+class PokeControllerController < ApplicationController
+end
---
+++ test/controllers/poke_controller_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PokeControllerControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
