f71a96fa0fc099d680e40b3c76c830b516075eec
Anthony Veronesi <tuffstuffmagee@gmail.com>
2019-06-19 18:19:22 -0400

    fixing java

---
+++ app/views/poke/_home.js
@@ -0,0 +1,10 @@
+<script>
+  $(function(){
+  //JSON, $.getJSON()
+  var pokeapi_url = "http://pokeapi.co/api/v2/generation/1";
+
+  $.getJSON(pokeapi_url).done(function(data){
+    console.log(data);
+  })
+});
+</script>
