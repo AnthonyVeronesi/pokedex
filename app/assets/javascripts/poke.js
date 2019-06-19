4ef3537005f908105795e3f4fa431c1eb43ee60b
Anthony Veronesi <tuffstuffmagee@gmail.com>
2019-06-19 18:25:46 -0400

    fixes

--- app/assets/javascripts/poke.js
+++ app/assets/javascripts/poke.js
@@ -1,17 +1,27 @@
+$(function(){
+  //JSON, $.getJSON()
+  var pokeapi_url = "http://pokeapi.co/api/v2/generation/1";
 
-var request = new XMLHttpRequest()
-function pageLoad()
-{
-  console.log('i got here')
-  request.open('GET', 'https://pokeapi.co/api/v2/generation/1', true)
-};
-request.onload = function() {
-  var poke_data JSON.parse(this.response)
-
-  poke_data.forEach(pokemon_species => {
-  //logging each pokemon name
-  console.log(pokemon_species.name)
+  $.getJSON(pokeapi_url).done(function(data){
+    console.log(data);
   })
-}
+});
 
-request.send()
+
+
+// var request = new XMLHttpRequest()
+// function pageLoad()
+// {
+//   console.log('i got here')
+//   request.open('GET', 'https://pokeapi.co/api/v2/generation/1', true)
+// };
+// request.onload = function() {
+//   var poke_data JSON.parse(this.response)
+//
+//   poke_data.forEach(pokemon_species => {
+//   //logging each pokemon name
+//   console.log(pokemon_species.name)
+//   })
+// }
+//
+// request.send()
